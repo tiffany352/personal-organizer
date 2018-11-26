be19d099255de461b235883d47635d728bef6737
Tiffany Bennett <tiffnixen@gmail.com>
2018-11-25 21:27:27 -0800

    Add koa-router

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,24 @@
 import Koa from 'koa'
 import foo from './test'
+import Router from 'koa-router'
 import koaWebpack from 'koa-webpack'
 
 console.log("hello, backend")
 const app = new Koa()
+const router = new Router({
+  prefix: '/api'
+})
 
-koaWebpack().then((middleware) =>
-  app.use(middleware))
+router.get('/test', (ctx) => {
+  ctx.body = {
+    result: 'hello, world'
+  }
+})
 
-/*app.use(async ctx => {
-  ctx.body = foo()
-})*/
-
-app.listen(3000)
+koaWebpack().then((middleware) => {
+  app
+    .use(middleware)
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(3000)
+})
