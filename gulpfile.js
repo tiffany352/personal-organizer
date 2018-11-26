52683d482d1a5e507586db0737621c85b3e529ca
Tiffany Bennett <tiffnixen@gmail.com>
2018-11-25 21:46:24 -0800

    Add gulp start task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var nodemon = require("gulp-nodemon")
 var tsProject = ts.createProject("tsconfig.json");
 
 function compileTypescript() {
@@ -8,4 +9,15 @@ function compileTypescript() {
     .js.pipe(gulp.dest("dist"));
 }
 
+function start() {
+  nodemon({
+    script: 'dist/app.js',
+    watch: ["server/"],
+    ext: 'js,ts,tsx,json,jsx',
+    tasks: ['compileTypescript']
+  })
+}
+
 exports.default = compileTypescript
+exports.compileTypescript = compileTypescript
+exports.start = gulp.series(compileTypescript, start)
